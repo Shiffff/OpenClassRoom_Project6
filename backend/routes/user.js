a5f930106a1f330e4eac717c421e134b7ff26a4a
shiffff <alexis.privat@live.fr>
2022-07-20 16:27:21 +0200

    password validator

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
+const password = require('../middleware/password')
 
 
-router.post('/signup', userCtrl.signup);    // route uniquement (logique importé via controllers)
+router.post('/signup', password, userCtrl.signup);    // route uniquement (logique importé via controllers)
 router.post('/login', userCtrl.login);      
 
 module.exports = router;        // exportation pour pouvoir l'importé dans app.Js
