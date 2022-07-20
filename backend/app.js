275b72a650e38aadcfd6a062961135211c9af2c1
shiffff <alexis.privat@live.fr>
2022-07-20 16:38:10 +0200

    rate limit

--- backend/app.js
+++ backend/app.js
@@ -14,10 +14,11 @@ var cors = require('cors')      //Gere le pb de cors (ystème de sécurité qui,
 
 const helmet = require('helmet') // définie et peu caché certaines information dans les en-têtes HTTP
 
-const mongoSanitize = require('express-mongo-sanitize');
+const rateLimit = require('express-rate-limit');    //La limitation du débit empêche la même adresse IP de faire trop de demandes
 
+const mongoSanitize = require('express-mongo-sanitize'); //fonction de nettoyage des entrées supprimera toutes les clés commençant par '$' 
 
-require('dotenv').config();
+require('dotenv').config();         // variable d'environement ( stockage local de mot de passe)
 
 
 
@@ -27,6 +28,12 @@ mongoose.connect(`mongodb+srv://${process.env.id}:${process.env.password}@projec
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+  const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 min
+    max: 100, // limite chaque IP à 100 requêtes par window de 15min
+    standardHeaders: true, // retourne l'info de limite dans les headers
+    legacyHeaders: false // désactive le 'X-rateLimit-*' headers
+});
 
   app.use(cors());                                        //Gere le pb de cors (ystème de sécurité qui, par défaut, bloque les appels HTTP entre des serveurs différents)
   
